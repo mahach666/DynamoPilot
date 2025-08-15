7194b0b78035de6797922e800016b964d7a7c1cd
Смирнов <maxah66@gmail.com>
2025-08-15 15:31:06 +0300

    AddParsers

---
+++ src/DynamoPilot.Zero/UserState/Parsers.cs
@@ -0,0 +1,28 @@
+﻿using Ascon.Pilot.SDK;
+using Dynamo.Graph.Nodes;
+using DynamoPilot.Data.Wrappers;
+using System;
+
+namespace UserState
+{
+    public static class Parsers
+    {
+        [IsDesignScriptCompatible]
+        public static Guid GetId(PUserState pUserState) => pUserState.Id;
+
+        [IsDesignScriptCompatible]
+        public static string GetName(PUserState pUserState) => pUserState.Name;
+
+        [IsDesignScriptCompatible]
+        public static  string GetTitle(PUserState pUserState) => pUserState.Title;
+
+        [IsDesignScriptCompatible]
+        public static byte[] GetIcon(PUserState pUserState) => pUserState.Icon;
+
+        [IsDesignScriptCompatible]
+        public static UserStateColorNames GetColor(PUserState pUserState) => pUserState.Color;
+
+        [IsDesignScriptCompatible]
+        public static bool GetIsDeleted(PUserState pUserState) => pUserState.IsDeleted;
+    }
+}
