1e8c014612a1928d83a8eef53db26a65c83b4e95
Смирнов <maxah66@gmail.com>
2025-08-16 18:20:20 +0300

    fix

--- src/DynamoPilot.Zero/ChatMessage/Properties.cs
+++ src/DynamoPilot.Zero/ChatMessage/Properties.cs
@@ -4,6 +4,7 @@ using DynamoPilot.Data.Wrappers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace ChatMessage
 {
@@ -13,7 +14,10 @@ namespace ChatMessage
         public static Guid GetId(PChatMessage pChatMessage) => pChatMessage.Id;
 
         [IsDesignScriptCompatible]
-        public static byte[] GetData(PChatMessage pChatMessage) => pChatMessage.Data;
+        public static byte[] GetByteData(PChatMessage pChatMessage) => pChatMessage.Data;
+
+        [IsDesignScriptCompatible]
+        public static string GetStringData(PChatMessage pChatMessage) => Encoding.UTF8.GetString(pChatMessage.Data);
 
         [IsDesignScriptCompatible]
         public static int GetCreatorId(PChatMessage pChatMessage) => pChatMessage.CreatorId;
@@ -34,6 +38,14 @@ namespace ChatMessage
         public static MessageType GetType(PChatMessage pChatMessage) => pChatMessage.Type;
 
         [IsDesignScriptCompatible]
-        public static List<PChatMessage> GetRelatedMessages(PChatMessage pChatMessage) => pChatMessage.RelatedMessages.Select(i => new PChatMessage((IChatMessage)i.Unwrap())).ToList();
+        public static List<PChatMessage> GetRelatedMessages(PChatMessage pChatMessage) 
+            => pChatMessage.RelatedMessages
+            .Select(i => new PChatMessage((IChatMessage)i.Unwrap()))
+            .ToList();
+
+        //public static string GetStringData(PChatMessage pChatMessage)
+        //{
+        //    return pChatMessage.GetData<string>();
+        //}
     }
 }
